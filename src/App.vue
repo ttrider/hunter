080202e88ebfe488bd486276859c41770d687439
Vladimir Yangurskiy <vladyang@amazon.com>
2021-04-20 16:54:37 -0700

    auth 2

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@
         >Login</GoogleLogin
       >
       <GoogleLogin :params="gParams" :logoutButton="true">Logout</GoogleLogin>
+      <div>{{ userName }}</div>
     </header>
 
     <footer class="footer">
@@ -51,30 +52,57 @@
 import { Component, Vue } from "vue-property-decorator";
 import { loadDropedFile, saveLocalFile } from "@/store/app";
 import GoogleLogin from "vue-google-login";
+import { LoaderPlugin } from "vue-google-login";
 import AWS from "aws-sdk";
 // import Amplify, { Auth } from "aws-amplify";
+
+Vue.use(LoaderPlugin, {
+  client_id:
+    "138993422227-h19aliqjhes1rmqcvnkkufsaiq7r9gv5.apps.googleusercontent.com",
+});
 
 @Component({
   name: "App",
   components: { GoogleLogin },
 })
 export default class App extends Vue {
+  created() {
+    (Vue as any).GoogleAuth.then((auth2: any) => {
+      console.log("signed-in:");
+      console.log(auth2.isSignedIn.get());
+      console.log("current user-in:");
+      console.log(auth2.currentUser.get());
+    });
+  }
+
+  userName = "<none>";
+
   gSuccess(googleUser: any) {
-    console.log(googleUser);
-    console.log(googleUser.getBasicProfile());
-    console.log(googleUser.getAuthResponse(true));
+    const profile = googleUser.getBasicProfile();
 
-    // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    //   IdentityPoolId: "us-east-1:0799a107-61fb-46be-8d7b-e3ef74f644a5",
-    //   Logins: {
-    //     "accounts.google.com": googleUser["id_token"],
-    //   },
-    // });
+    console.info(profile.getId());
+    console.info(profile.getName());
+    console.info(profile.getGivenName());
+    console.info(profile.getFamilyName());
+    console.info(profile.getImageUrl());
+    console.info(profile.getEmail());
 
-    // // Obtain AWS credentials
-    // console.info(AWS.config.credentials.accessKeyId);
-    // console.info(AWS.config.credentials.secretAccessKey);
-    // console.info(AWS.config.credentials.sessionToken);
+    this.userName = profile.getName();
+
+    const auth = googleUser.getAuthResponse(true);
+    const id_token = auth.id_token;
+    console.info(profile.getEmail(id_token));
+
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: "us-east-1:0799a107-61fb-46be-8d7b-e3ef74f644a5",
+      Logins: {
+        "accounts.google.com": id_token,
+      },
+    });
+
+    console.info(AWS.config.credentials.accessKeyId);
+    console.info(AWS.config.credentials.secretAccessKey);
+    console.info(AWS.config.credentials.sessionToken);
 
     // Amplify.configure({})
 
