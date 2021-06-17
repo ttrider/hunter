484ead560d057828b01854d29b44cc01256c000a
Vladimir Yangurskiy <vladyang@amazon.com>
2021-06-16 18:32:17 -0700

    client

--- src/store/client.ts
+++ src/store/client.ts
@@ -3,11 +3,23 @@ import localforage from "localforage";
 import store from ".";
 import { AppModule } from "./app";
 import { AuthModule } from "./auth";
-import { AssetRecord, ContactRecord, EventRecord, SessionInfo } from "./model";
-import uuid from "uuid";
-import { requestDocuments } from "@/client";
+import {
+  AssetRecord,
+  CompanyRecord,
+  ContactRecord,
+  EventRecord,
+  InterviewRecord,
+  SessionInfo,
+  TaskRecord,
+} from "./model";
 import { DocumentClient } from "@/client/documentClient";
-import { PositionRecord } from "./positions";
+import { PositionRecord, PositionsModule } from "./positions";
+import { AssetsModule } from "./assets";
+import { ContactsModule } from "./contacts";
+import { EventsModule } from "./events";
+import { InterviewsModule } from "./interviews";
+import { TasksModule } from "./tasks";
+//import { CompaniesModule } from "./companies";
 
 export const localStore = localforage.createInstance({ name: "localFile" });
 // export const contactsStore = localforage.createInstance({ name: "contacts" });
@@ -86,54 +98,80 @@ export async function get(commit?: string) {
   return ld;
 }
 
-export const contactsClient = new DocumentClient<ContactRecord>(
-  "contacts",
-  "contacts/initialize",
-  "contacts/update",
-  async (client) => {
-    const documents = await requestDocuments<ContactRecord>(
-      "contacts",
-      client.lastUpdated ? client.lastUpdated : undefined
-    );
-    return documents ?? {};
-  }
-);
+export const assetsClient = DocumentClient.create<AssetRecord>("assets");
 
-export const positionsClient = new DocumentClient<PositionRecord>(
-  "positions",
-  "positions/initialize",
-  "positions/update",
-  async (client) => {
-    const documents = await requestDocuments<PositionRecord>(
-      "positions",
-      client.lastUpdated ? client.lastUpdated : undefined
-    );
-    return documents ?? {};
-  }
+export const companiesClient = DocumentClient.create<CompanyRecord>(
+  "companies"
 );
+export const contactsClient = DocumentClient.create<ContactRecord>("contacts");
+export const eventsClient = DocumentClient.create<EventRecord>("events");
+export const interviewsClient = DocumentClient.create<InterviewRecord>(
+  "interviews"
+);
+export const positionsClient = DocumentClient.create<PositionRecord>(
+  "positions"
+);
+export const tasksClient = DocumentClient.create<TaskRecord>("tasks");
 
-export const eventsClient = new DocumentClient<EventRecord>(
-  "events",
-  "events/initialize",
-  "events/update",
-  async (client) => {
-    const documents = await requestDocuments<EventRecord>(
-      "events",
-      client.lastUpdated ? client.lastUpdated : undefined
-    );
-    return documents ?? {};
-  }
-);
+export const clients = {
+  assets: assetsClient,
+  //companies: companiesClient,
+  contacts: contactsClient,
+  events: eventsClient,
+  interviews: interviewsClient,
+  positions: positionsClient,
+  tasks: tasksClient,
+};
 
-export const assetsClient = new DocumentClient<AssetRecord>(
-  "assets",
-  "assets/initialize",
-  "assets/update",
-  async (client) => {
-    const documents = await requestDocuments<AssetRecord>(
-      "assets",
-      client.lastUpdated ? client.lastUpdated : undefined
-    );
-    return documents ?? {};
+export function initialize() {
+  // const clientSet = itemSetToArray((clients as unknown) as ItemSet<BaseClient>);
+  // const tasks = clientSet.map((client) => client.initialize());
+  // return Promise.all(tasks);
+  return Promise.resolve();
+}
+export function refresh() {
+  // const clientSet = itemSetToArray((clients as unknown) as ItemSet<BaseClient>);
+  // const tasks = clientSet.map((client) => client.refresh());
+  // return Promise.all(tasks);
+  return Promise.resolve();
+}
+export function reset() {
+  // const clientSet = itemSetToArray((clients as unknown) as ItemSet<BaseClient>);
+  // const tasks = clientSet.map((client) => client.reset());
+  // return Promise.all(tasks);
+  return Promise.resolve();
+}
+
+export async function loadFromSessionInfo(session: SessionInfo) {
+  if (session.assets) {
+    await assetsClient.update(session.assets);
   }
-);
+  if (session.companies) {
+    await companiesClient.update(session.companies);
+  }
+  if (session.contacts) {
+    await contactsClient.update(session.contacts);
+  }
+  if (session.events) {
+    await eventsClient.update(session.events);
+  }
+  if (session.interviews) {
+    await interviewsClient.update(session.interviews);
+  }
+  if (session.positions) {
+    await positionsClient.update(session.positions);
+  }
+  if (session.tasks) {
+    await tasksClient.update(session.tasks);
+  }
+}
+
+export function exportToSessionInfo(session: SessionInfo) {
+  session.assets = AssetsModule.items;
+  //session.companies = CompaniesModule.items;
+  session.contacts = ContactsModule.items;
+  session.events = EventsModule.items;
+  session.interviews = InterviewsModule.items;
+  session.positions = PositionsModule.items;
+  session.tasks = TasksModule.items;
+}
