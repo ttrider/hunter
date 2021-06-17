a7e8945807abbab6daa9498b733668b3433a354c
Vladimir Yangurskiy <vladyang@amazon.com>
2021-06-16 18:37:32 -0700

    store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,27 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import { AppState } from "./app";
+import { AssetsState } from "./assets";
 import { AuthState } from "./auth";
+import { CompaniesState } from "./companies";
 import { ContactsState } from "./contacts";
+import { EventsState } from "./events";
+import { InterviewsState } from "./interviews";
 import { PositionsState } from "./positions";
+import { TasksState } from "./tasks";
 
 Vue.use(Vuex);
 
 export interface RootState {
   app: AppState;
   auth: AuthState;
+  assets: AssetsState;
+  companies: CompaniesState;
   contacts: ContactsState;
+  events: EventsState;
+  interviews: InterviewsState;
   positions: PositionsState;
+  tasks: TasksState;
 }
 
 const store = new Vuex.Store<RootState>({});
